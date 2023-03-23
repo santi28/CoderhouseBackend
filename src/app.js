7a84bd31473aa148f45c3250cd585b2b5ba0c2f0
Santiago de Nicolás <santidenicolas@gmail.com>
2023-03-23 20:10:34 -0300

    test app.js

--- src/app.js
+++ src/app.js
@@ -1,42 +1,12 @@
-import http from 'http'
 import express from 'express'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
 
-// Import app config
-import config from './config/app.config.js'
+const app = express()
+const PORT = process.env.PORT || 3000
 
-// Import of initializers
-import { initMongoDB } from './config/mongodb.config.js'
-import { initWebscoketServer } from './websocket/app.js'
-import { initHandlebars } from './config/templateEnginge.config.js'
-import { initSessions } from './config/sessions.config.js'
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-import Router from './router/index.router.js'
-import initPassport from './config/passport.config.js'
-import { loggerMiddleware } from './middlewares/logger.middleware.js'
-
-const app = express() // Inicializa el servidor express
-const server = http.createServer(app) // Inicializa el servidor http
-const PORT = config.app.port // Configura el puerto, si no existe usa el 3000
-
-dotenv.config() // Inicializa las variables de entorno
-// app.use(morgan('dev')) // Inicializa el logger utilizando morgan
-app.use(loggerMiddleware) // Inicializa el logger utilizando winston
-app.use(express.static('./src/public')) // Configura la carpeta de estaticos
-
-initMongoDB() // Inicializa la conexion a la base de datos
-initWebscoketServer(server) // Inicializa el servidor de websockets
-initHandlebars(app) // Inicializa el motor de plantillas
-initSessions(app) // Inicializa las sesiones
-initPassport(app) // Inicializa el sistema de autenticacion por passport
-
-app.use(express.json()) // Configura el body parser para json
-app.use(express.urlencoded({ extended: true })) // Configura el body parser para formularios
-
-app.use(Router) // Configura el enrutador
-
-server.listen(
-  PORT,
-  console.log(`🚀 Server running on url http://localhost:${PORT}`)
-)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
