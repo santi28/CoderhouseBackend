2067f2094280b34d1d9685b4b2613f7e6a26efac
Santiago de Nicolás <santidenicolas@gmail.com>
2023-03-29 20:18:17 -0300

    :card_file_box: Added configuration and mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log('Welcome to the TypeScript App!')
+import express, { } from 'express'
+import connectToDatabase from './config/mongodb.config'
+import configurations from './config/app.config'
+
+void (async () => {
+  // Conectamos a la base de datos
+  await connectToDatabase()
+
+  // Inicializamos la aplicación
+  const app = express()
+
+  // Definimos el puerto de escucha
+  const port = configurations.port
+
+  // Iniciamos el servidor
+  app.listen(
+    port,
+    () => console.log(`🚀 Server running at http://localhost:${port}`)
+  )
+})()
